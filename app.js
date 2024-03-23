93acbddf12e07f674eac3467e883199528149787
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-03-23 09:54:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+let personName = '';
+personName = prompt('what is your name?') || '';
+if (personName !== null && personName !== '')
+    ;
+{
+    alert;
+    {
+        `hello ${personName} would you like to learn some python today?`;
+    }
+}
+{
+    alert;
+    {
+        'you have to write your name!';
+    }
+}
